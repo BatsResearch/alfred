1541da5f483b9bc66f3a8819bebc71a87e923cae
Peilin Yu <peilin_yu@brown.edu>
2023-04-06 18:33:44 -0400

    Fixing model specification

--- alfred/fm/openai.py
+++ alfred/fm/openai.py
@@ -19,7 +19,7 @@ OPENAI_MODELS = (
     "gpt-4-32k-0314",
     "gpt-3.5-turbo",
     "gpt-3.5-turbo-0301",
-    "text-davinci-003"
+    "text-davinci-003",
     "text-davinci-002",
     "text-davinci-001",
     "text-curie-001",
