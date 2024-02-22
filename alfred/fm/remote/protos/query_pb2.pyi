aeb4928fe307be5518263a6edb3e8396c2f39216
Peilin Yu <peilin_yu@brown.edu>
2024-02-22 14:25:34 -0500

    Add: Compatibility with  Newer Protobuf Version

--- alfred/fm/remote/protos/query_pb2.pyi
+++ alfred/fm/remote/protos/query_pb2.pyi
@@ -4,44 +4,44 @@ from typing import ClassVar as _ClassVar, Optional as _Optional
 
 DESCRIPTOR: _descriptor.FileDescriptor
 
-class EncodeRequest(_message.Message):
-    __slots__ = ["kwargs", "message", "reduction"]
+class RunRequest(_message.Message):
+    __slots__ = ("message", "candidate", "kwargs")
+    MESSAGE_FIELD_NUMBER: _ClassVar[int]
+    CANDIDATE_FIELD_NUMBER: _ClassVar[int]
     KWARGS_FIELD_NUMBER: _ClassVar[int]
+    message: str
+    candidate: str
+    kwargs: str
+    def __init__(self, message: _Optional[str] = ..., candidate: _Optional[str] = ..., kwargs: _Optional[str] = ...) -> None: ...
+
+class RunResponse(_message.Message):
+    __slots__ = ("message", "ranked", "success", "logit", "embedding")
+    MESSAGE_FIELD_NUMBER: _ClassVar[int]
+    RANKED_FIELD_NUMBER: _ClassVar[int]
+    SUCCESS_FIELD_NUMBER: _ClassVar[int]
+    LOGIT_FIELD_NUMBER: _ClassVar[int]
+    EMBEDDING_FIELD_NUMBER: _ClassVar[int]
+    message: str
+    ranked: bool
+    success: bool
+    logit: str
+    embedding: bytes
+    def __init__(self, message: _Optional[str] = ..., ranked: bool = ..., success: bool = ..., logit: _Optional[str] = ..., embedding: _Optional[bytes] = ...) -> None: ...
+
+class EncodeRequest(_message.Message):
+    __slots__ = ("message", "reduction", "kwargs")
     MESSAGE_FIELD_NUMBER: _ClassVar[int]
     REDUCTION_FIELD_NUMBER: _ClassVar[int]
-    kwargs: str
+    KWARGS_FIELD_NUMBER: _ClassVar[int]
     message: str
     reduction: str
+    kwargs: str
     def __init__(self, message: _Optional[str] = ..., reduction: _Optional[str] = ..., kwargs: _Optional[str] = ...) -> None: ...
 
 class EncodeResponse(_message.Message):
-    __slots__ = ["embedding", "success"]
+    __slots__ = ("embedding", "success")
     EMBEDDING_FIELD_NUMBER: _ClassVar[int]
     SUCCESS_FIELD_NUMBER: _ClassVar[int]
     embedding: bytes
     success: bool
     def __init__(self, embedding: _Optional[bytes] = ..., success: bool = ...) -> None: ...
-
-class RunRequest(_message.Message):
-    __slots__ = ["candidate", "kwargs", "message"]
-    CANDIDATE_FIELD_NUMBER: _ClassVar[int]
-    KWARGS_FIELD_NUMBER: _ClassVar[int]
-    MESSAGE_FIELD_NUMBER: _ClassVar[int]
-    candidate: str
-    kwargs: str
-    message: str
-    def __init__(self, message: _Optional[str] = ..., candidate: _Optional[str] = ..., kwargs: _Optional[str] = ...) -> None: ...
-
-class RunResponse(_message.Message):
-    __slots__ = ["embedding", "logit", "message", "ranked", "success"]
-    EMBEDDING_FIELD_NUMBER: _ClassVar[int]
-    LOGIT_FIELD_NUMBER: _ClassVar[int]
-    MESSAGE_FIELD_NUMBER: _ClassVar[int]
-    RANKED_FIELD_NUMBER: _ClassVar[int]
-    SUCCESS_FIELD_NUMBER: _ClassVar[int]
-    embedding: bytes
-    logit: str
-    message: str
-    ranked: bool
-    success: bool
-    def __init__(self, message: _Optional[str] = ..., ranked: bool = ..., success: bool = ..., logit: _Optional[str] = ..., embedding: _Optional[bytes] = ...) -> None: ...
